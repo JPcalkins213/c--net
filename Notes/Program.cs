20264e8cf46506b904c687c740d1010e206e6b44
Jessica Calkins <jessicacalkins795213@gmail.com>
2021-10-05 07:20:26 -0500

    fundementals

---
+++ Notes/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+// namespace Notes
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+
+// if and elseif statements
+// int numRings = 5;
+// if(numRings>=5)
+// {
+//     Console.WriteLine("You are welcome to join the party");
+// }
+// else if
+// {
+//     Console.WriteLine($"Decent...but {numRings} rings aren't enough")
+// }
+// else
+// {
+//     Console.WriteLine("Go win some more rings")
+// }
+
+//loop from 1 to 5 including 5
+
+// for (int i = 1; i <= 5; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// //loop from 1 to 5 excluding 5 
+
+// for (int i = 1; i < 5; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+//while loops
+
+// int i = 1;
+// while(i<6)
+// {
+//     Console.WriteLine(i);
+//     i = i+1;
+// }
+
+//Generating random values
+//first create a randome object
+
+// Random rand = new Random();
+//to generate 10 random numbers between 8 and 2 we call .Next(2,8) in a for loop
+// for (int val = 0; val < 10; val++)
+// {
+//     Console.WriteLine(rand.Next(2,8));
+// }
---
+++ fundemental1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+// for (int i = 1; i <= 255; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// for (int i = 1; i < 100; i++)
+// {
+//     if (i%5 == 0 && i%3 == 0)
+//     {
+//         Console.WriteLine("nope");
+//     }
+//     else if(i%5==0)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     else if(i%3==0)
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+
+// for (int i = 1; i < 100; i++)
+// {
+//     if (i%5 == 0 && i%3 == 0)
+//     {
+//         Console.WriteLine("FizzBuzz");
+//     }
+//     else if(i%5==0)
+//     {
+//         Console.WriteLine("Buzz");
+//     }
+//     else if(i%3==0)
+//     {
+//         Console.WriteLine("Fizz");
+//     }
+// }
+
+//turning the previous for loop into a while loop
+int num = 1;
+while(num<=100)
+{
+    num=num+1;
+    if (num%5 == 0 && num%3 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(num%5==0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if(num%3==0)
+    {
+        Console.WriteLine("Fizz");
+    }
+}
